d0d3c0991fb6a218abba08585571dc875242c478
vr3n <vickyspatel@gmail.com>
2020-05-03 17:18:48 +0530

    -

--- properties/properties/items.py
+++ properties/properties/items.py
@@ -6,9 +6,31 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
 
 class PropertiesItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    # Primary fields
+    title = Field()
+    location = Field()
+    price = Field()
+    description = Field()
+    carpet_area = Field()
+    status = Field()
+    transaction = Field()
+    furnishing = Field()
+    society = Field()
+    image_urls = Field()
+
+    # calculated Fields
+    images = Field()
+
+    # HouseKeeping Fields.
+    url = Field()
+    project = Field()
+    spider = Field()
+    server = Field()
+    date = Field()
--- properties/properties/settings.py
+++ properties/properties/settings.py
@@ -17,6 +17,7 @@ NEWSPIDER_MODULE = 'properties.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'properties (+http://www.yourdomain.com)'
+USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -39,34 +40,34 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'properties.middlewares.PropertiesSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'properties.middlewares.PropertiesDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'properties.pipelines.PropertiesPipeline': 300,
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
